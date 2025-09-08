88a574fa2e5bb47292854c7dc54da76d13061cc0
Soumyadeepbasak037 <soumyadeep.basak06@gmail.com>
2025-09-08 09:33:29 +0530

    Added routes controller

---
+++ middlewares/routesContoller.js
@@ -0,0 +1,38 @@
+import db from "../config/db.js";
+
+export const insertStops = async (req, res) => {
+  try {
+    const { jsonData } = req.body; //[{stopname:,longitude:,lat:},{stopname:,longitude:,lat:},{stopname:,longitude:,lat:}] array of objects
+    //const stops = JSON.parse(jsonData);
+    const stops = jsonData;
+    const insertIntoStopsQuery = `INSERT INTO STOPS (stop_name,location) VALUES ($1,
+                          ST_SetSRID(ST_MakePoint($2, $3), 4326)::geography)
+                          RETURNING stop_id`;
+
+    let response_str = "";
+    for (const stop of stops) {
+      const result = await db.query(insertIntoStopsQuery, [
+        stop.stopname,
+        stop.longitude,
+        stop.latitude,
+      ]);
+      response_str += ` ${stop.stopname}(id=${result.rows[0].stop_id})`;
+    }
+    res.json({
+      message: `These stops were successfully inserted ${response_str}`,
+    });
+  } catch (err) {
+    res.json({ message: err });
+  }
+};
+
+export const insertRouteStops = async (req, res) => {
+  try {
+    const { vehicle_no, stop_order_json } = req.body;
+    const insertQuery = ``;
+  } catch (err) {
+    res.json({ message: err });
+  }
+};
+
+const manageRoutestable = async () => {};
--- sockets/driverSocket.js
+++ sockets/driverSocket.js
@@ -4,7 +4,7 @@ const driverHandler = async (io, socket) => {
   socket.join(socket.vehicleId);
   console.log(`Driver ${socket.userId} joined vehicle ${socket.vehicleId}`);
 
-  vehicleNumberQuery = `SELECT vehicle_number, from vehicles where vehicle_id $1`;
+  vehicleNumberQuery = `SELECT vehicle_number from vehicles where vehicle_id $1`;
 
   result = await db.query(vehicleNumberQuery, [socket.vehicleId]);
 };
