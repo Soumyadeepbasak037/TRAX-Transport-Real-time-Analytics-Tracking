605f68449a8ab7a398b7c27106a0ec0b580678ef
Soumyadeepbasak037 <soumyadeep.basak06@gmail.com>
2025-11-02 18:35:32 +0530

    Env files added for pushing to prod

--- config/db.js
+++ config/db.js
@@ -1,12 +1,37 @@
-import pkg from "pg";
-const { Pool } = pkg;
+// import pkg from "pg";
+// import dotenv from "dotenv";
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "TRAX",
-  password: "new_password",
-  port: 5432,
-});
+// // dotenv.config();
+
+// const { Pool } = pkg;
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "TRAX",
+//   password: "new_password",
+//   port: 5432,
+// });
+
+//export default pool;
+
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+const { Pool } = pkg;
+// console.log(process.env);
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    })
+  : new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    });
 
 export default pool;
