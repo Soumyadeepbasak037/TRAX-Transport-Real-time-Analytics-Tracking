05b2bd88b7f442f26ea9a3d3736fd13d5ee432b0
Soumyadeepbasak037 <soumyadeep.basak06@gmail.com>
2025-11-04 02:14:20 +0530

    Added proper role based route access

--- routes/routesRoutes.js
+++ routes/routesRoutes.js
@@ -1,21 +1,28 @@
 import express from "express";
 import * as routes_controller from "../controllers/routesController.js";
 import auth_middleware from "../middlewares/authMiddleware.js";
-// import roleMiddleware from "../middlewares/roleValidationMiddleware.js";
+import roleMiddleware from "../middlewares/roleValidationMiddleware.js";
 const router = express.Router();
 
 // add role validation from roleValidationmiddleware only drivers can add routes
-router.post("/insertStops", auth_middleware, routes_controller.createStops);
+router.post(
+  "/insertStops",
+  auth_middleware,
+  roleMiddleware("admin"),
+  routes_controller.createStops
+);
 
 router.post(
   "/insertRouteStops",
   auth_middleware,
+  roleMiddleware("admin"),
   routes_controller.createRoute
 );
 
 router.post(
   "/updateRouteStops",
   auth_middleware,
+  roleMiddleware("admin"),
   routes_controller.updateRouteStops
 );
 export default router;
