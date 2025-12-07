c4e898b702a104f0be1d876eab10cbe15e3ba4d5
Soumyadeepbasak037 <soumyadeep.basak06@gmail.com>
2025-12-08 00:33:37 +0530

    neon-pg

--- config/db.js
+++ config/db.js
@@ -15,16 +15,39 @@
 
 //export default pool;
 
+// import pkg from "pg";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+// const { Pool } = pkg;
+// // console.log(process.env);
+// const pool = process.env.DATABASE_URL
+//   ? new Pool({
+//       connectionString: process.env.DATABASE_URL,
+//       ssl: { rejectUnauthorized: true },
+//     })
+//   : new Pool({
+//       user: process.env.DB_USER,
+//       host: process.env.DB_HOST,
+//       database: process.env.DB_NAME,
+//       password: process.env.DB_PASSWORD,
+//       port: process.env.DB_PORT,
+//     });
+
+// export default pool;
+//------------------------------------------
+
 import pkg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 const { Pool } = pkg;
-// console.log(process.env);
+
 const pool = process.env.DATABASE_URL
   ? new Pool({
       connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
+      ssl: { require: true },
     })
   : new Pool({
       user: process.env.DB_USER,
